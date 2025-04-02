9db398fad61ea26ffa6f1bac878aa1f77e4de87b
Tommy Oh <tommy32666@gmail.com>
2025-04-02 14:31:22 -0700

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://tommyoh0428.github.io",
+  url: "https://TommyOh0428.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/class-note",
+  baseUrl: "/class-note/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
